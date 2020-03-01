a56e606ad17ec56630c71bb85e47fd55f8aad92d
Keerthika Rajvel <keerthikarajvel@gmail.com>
2020-02-29 19:13:27 -0700

    Project 3

---
+++ Project 3/RentalCar.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.*;
+
+public class RentalStore{
+    Integer day;
+    Integer day_amnt;
+    List<Object> customers = new ArrayList<Object>();
+    List<Object> cars = new ArrayList<Object>();
+
+    public void check_new(){
+
+
+
+
+
+    }
+    
+}
+
+
+public interface Car{
+    void create_car();
+}
+
+public class Economy implements Car{
+    @override
+    public void create_car(){
+        
+    }
+}
+
+
+public static void main(String[] args) throws FileNotFoundException {
+    int days = 35;
+
+}
