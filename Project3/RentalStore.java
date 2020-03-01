665aad6a09d2c6fc8142c1663ac5cf3025c9eed0
Swarnalatha Natarajan <35426480+SwarnaLathaNatarajan@users.noreply.github.com>
2020-02-29 21:00:16 -0700

    Add files via upload

---
+++ Project3/RentalStore.java
@@ -0,0 +1,66 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.*;
+import java.util.Random;
+
+public class RentalStore extends Observable {
+    int day;
+    int day_amnt;
+    List<customerRecord> customers;
+    List<Object> cars;
+
+    public RentalStore (int day) //Initial setup : instantiate cars and customers
+    {
+        this.day = day;
+        this.day_amnt = 0;
+        this.cars = new ArrayList<Object>();
+        //Instantiate cars using factory
+        this.setupCars();
+        this.customers = new ArrayList<customerRecord>();
+        this.setupCustomers();//Instantiating customers
+    }
+    public void setupCars()
+    {
+        String[] carType = new String[]{"Economy", "Luxury", "Standard","SUV","Minivan"};
+        Random rand = new Random(); // adapted from https://mkyong.com/java/java-generate-random-integers-in-a-range/
+        int r;
+        CarFactory carFactory = new CarFactory();
+        Car car;
+        for (int i = 1; i <= 24; i++) {
+            r = rand.nextInt(((carType.length-1) - 0) + 1) + 0; //Randomizing the customer's type
+            car=carFactory.getType(carType[r]);
+            this.cars.add(car);
+        }
+    }
+    public void setupCustomers() {
+        String[] customerType = new String[]{"Casual", "Regular", "Business"};
+        Random rand = new Random(); // adapted from https://mkyong.com/java/java-generate-random-integers-in-a-range/
+        int r;
+        for (int i = 1; i <= 12; i++) {
+            r = rand.nextInt((2 - 0) + 1) + 0; //Randomizing the customer's type
+            this.customers.add(new customerRecord("C" + Integer.toString(i), customerType[r]));
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        //Redirecting System.out.println() output to a file.
+
+//        PrintStream o = new PrintStream(new File("carSimulator.out")); //adapted from https://www.geeksforgeeks.org/redirecting-system-out-println-output-to-a-file-in-java/
+//        System.setOut(o);
+//
+        RentalStore rentalStore = new RentalStore(0);
+
+        for(int i=0;i<rentalStore.cars.size();i++)
+        {
+            Object c= rentalStore.cars.get(i);
+            System.out.println(i);
+            System.out.println(c.getClass());
+        }
+//    for(int day=1;day<=35;day++)
+//    {
+//
+//    }
+    }
+}
