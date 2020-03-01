cddcbaa0c2aa87a6fbdda6f80536cbc1aeb545a5
Swarnalatha Natarajan <35426480+SwarnaLathaNatarajan@users.noreply.github.com>
2020-02-29 20:59:56 -0700

    Delete observer.java

--- observer.java
+++
@@ -1,9 +0,0 @@
-import java.util.Observable;
-import java.util.Observer;
-
-public class observer implements Observer {
-    @Override
-    public void update(Observable o, Object arg) {
-
-    }
-}
