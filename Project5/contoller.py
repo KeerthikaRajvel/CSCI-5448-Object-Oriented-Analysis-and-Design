b9256bd4140264ad725302958e84cce51741faa3
Swarnalatha Natarajan <swna2675@colorado.edu>
2020-04-05 23:40:24 -0600

    Flask server created

--- Project5/contoller.py
+++ Project5/contoller.py
@@ -3,10 +3,11 @@ from flask import Flask, render_template, request
 class Controller(object):
     def __init__(self):
         self.app=Flask(__name__)
+        self.port=3400
         @self.app.route('/')
         def index():
            return render_template('index.html')
 
 if __name__ == '__main__':
     controller=Controller()
-    controller.app.run(debug = True,port=3400)
+    controller.app.run(debug = True,port=controller.port)
