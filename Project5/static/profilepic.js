30e238fa09dc5642a28f615bc31d4aff583a6cb5
Swarnalatha Natarajan <swna2675@colorado.edu>
2020-04-07 15:03:37 -0600

    upload profile pic feature done

--- Project5/static/profilepic.js
+++ Project5/static/profilepic.js
@@ -1,22 +1,16 @@
-$(document).ready(function() {
-
-    var readURL = function(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-                $('.profile-pic').attr('src', e.target.result);
-            }
-
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
-
-    $(".file-upload").on('change', function(){
+$(document).ready(function(){
+// Prepare the preview for profile picture
+    $("#wizard-picture").change(function(){
         readURL(this);
     });
-
-    $(".upload-button").on('click', function() {
-       $(".file-upload").click();
-    });
 });
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#wizardPicturePreview').attr('src', e.target.result).fadeIn('slow');
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
