ee398d00b27ab5f3439a843cd7a37f58116b3ca0
Swarnalatha Natarajan <swna2675@colorado.edu>
2020-04-06 18:49:13 -0600

    Landing page done

--- Project5/contoller.py
+++ Project5/contoller.py
@@ -7,8 +7,13 @@ app = Flask(__name__)
 class Controller:
     def hello(self):
         return render_template('index.html')
-
+    def dashboard(self):
+        return render_template('dashboard.html')
+    def signup(self):
+        return render_template('signup.html')
 
 controller = Controller()
 app.add_url_rule('/', 'index', lambda: controller.hello())
+app.add_url_rule('/dashboard','dashboard', lambda: controller.dashboard(),methods=['POST'])
+app.add_url_rule('/signup','signup', lambda: controller.signup())
 app.run(debug = True,port=3400)
