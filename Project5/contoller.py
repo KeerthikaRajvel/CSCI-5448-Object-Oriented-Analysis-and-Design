b47df5d0f448771a3e2d9a058dec0a376fb75500
Swarnalatha Natarajan <swna2675@colorado.edu>
2020-04-16 20:58:37 -0600

    Model class created

--- Project5/contoller.py
+++ Project5/contoller.py
@@ -1,20 +1,13 @@
-import pymongo
-from pymongo import MongoClient
 from flask import Flask, render_template,request
+from model import Model
 
 app = Flask(__name__)
-try:
-    cluster = MongoClient("mongodb+srv://bunkbud:ooad@cluster0-xythq.mongodb.net/test?retryWrites=true&w=majority")
-    print("Connected!")
-except:
-    print("Counldnt connect!")
-db = cluster["bunkbud"]
-collection = db["users"]
-collection1 = db["findroom"]
-collection2 = db["fillroom"]
 
 class Controller:
     instance=None
+    model=None
+    port=None
+
     @staticmethod
     def getInstance():
         """ Static access method. """
@@ -28,85 +21,51 @@ class Controller:
             raise Exception("This class is a singleton!")
         else:
             Controller.instance = self
+            Controller.model = Model()
+            Controller.port = 3400
+
+    def add_url_rules(self):
+        app.add_url_rule('/', 'index', lambda: controller2.hello())
+        app.add_url_rule('/signup', 'signup', lambda: controller2.signup())
+        app.add_url_rule('/questionnaire', 'questionnaire', lambda: controller2.display_questionnaire(),methods=['POST'])
+        app.add_url_rule('/findroom_dashboard', 'findroom_dashboard',lambda: controller2.display_dashboard(findroom(controller1.model)), methods=['POST'])
+        app.add_url_rule('/fillroom_dashboard', 'fillroom_dashboard',lambda: controller2.display_dashboard(fillroom(controller1.model)), methods=['POST'])
 
     def hello(self):
         return render_template('index.html')
+
     def dashboard(self):
         return render_template('dashboard.html')
+
     def signup(self):
         return render_template('signup.html')
-    def aftersignup(self): #Model pattern
+
+    def display_questionnaire(self):
         if request.method == 'POST':
-            name = request.form['name']
-            email = request.form['email']
-            password = request.form['password']
-            # description = request.form['description']
-            gender = request.form['sex']
-            age = request.form['age']
-            diet = request.form['diet']
-            smoker = request.form['smoker']
-            drinker = request.form['drinker']
-            doc = {"name":name,"email":email,"password":password,"gender":gender,"Age":age,"diet":diet,"smoker":smoker,"drinker":drinker}
-            print("Document to be inserted: ",doc)
-            x = collection.insert_one(doc)
+            self.model.add_User(request.form)
             if request.form['signupButton'] == 'Find':
                 return render_template('findRoomQuestionnaire.html')
                 # return render_template('pass.html',name=name,email=email,password=password,description=description,gender=gender,age=age,diet=diet,smoker=smoker,drinker=drinker)
             if request.form['signupButton'] == 'Fill':
                 return render_template('fillRoomQuestionnaire.html')
-    def afterfindroom(self):
-        date = request.form['date']
-        rent = request.form['rent']
-        occupancy = request.form['tenants']
-        gender = request.form['sex']
-        age = request.form['age']
-        diet = request.form['diet']
-        smoker = request.form['smoker']
-        drinker = request.form['drinker']
-        music = request.form['music']
-        friends = request.form['friends']
-        doc = {"date":date,"rent":rent,"occupancy":occupancy,"gender":gender,"age":age,"diet":diet,"smoker":smoker,"drinker":drinker,"music":music,"friends":friends}
-        print("Document to be inserted: ",doc)
-        x = collection1.insert_one(doc)
-        return render_template('dashboard.html')
-        # return render_template('pass.html',date=date,rent=rent,occupancy=occupancy,gender=gender,age=age,diet=diet,smoker=smoker,drinker=drinker,music=music,friends=friends)
 
 
-    def afterfillroom(self):
-        date = request.form['date']
-        rent = request.form['rent']
-        room = request.form['room']
-        occupancy = request.form['tenants']
-        gender = request.form['sex']
-        age = request.form['age']
-        diet = request.form['diet']
-        smoker = request.form['smoker']
-        drinker = request.form['drinker']
-        music = request.form['music']
-        friends = request.form['friends']
-        doc = {"date":date,"rent":rent,"room":room,"occupancy":occupancy,"gender":gender,"age":age,"diet":diet,"smoker":smoker,"drinker":drinker,"music":music,"friends":friends}
-        print("Document to be inserted: ",doc)
-        x = collection2.insert_one(doc)
-        return render_template('dashboard.html')
-        # return render_template('pass.html',date=date,rent=rent,room=room,occupancy=occupancy,gender=gender,age=age,diet=diet,smoker=smoker,drinker=drinker,music=music,friends=friends)
+    def display_dashboard(self,action): #Strategy Pattern
+        return action
 
 
-#     def display_questionnaire(self,questionnaire): #Strategy Pattern
-#         return questionnaire
-#
-# def findRoomQuestionnaire():
-#     return render_template('findRoomQuestionnaire.html')
-# def fillRoomQuestionnaire():
-#     return render_template('fillRoomQuestionnaire.html')
+def findroom(model):
+    model.add_findroom(request.form)
+    return render_template('dashboard.html')
+    # return render_template('pass.html',date=date,rent=rent,occupancy=occupancy,gender=gender,age=age,diet=diet,smoker=smoker,drinker=drinker,music=music,friends=friends)
 
-controller1 = Controller()
-controller2 = Controller.getInstance() #Singleton Pattern
-app.add_url_rule('/', 'index', lambda: controller2.hello())
-app.add_url_rule('/dashboard','dashboard', lambda: controller2.dashboard(),methods=['POST'])
-app.add_url_rule('/signup','signup', lambda: controller2.signup())
-app.add_url_rule('/aftersignup','aftersignup', lambda: controller2.aftersignup(),methods=['POST'])
-app.add_url_rule('/afterfindroom','afterfindroom', lambda: controller2.afterfindroom(),methods=['POST'])
-app.add_url_rule('/afterfillroom','afterfillroom', lambda: controller2.afterfillroom(),methods=['POST'])
-# app.add_url_rule('/findRoom','findRoom', lambda: controller2.display_questionnaire(findRoomQuestionnaire()))
-# app.add_url_rule('/fillRoom','fillRoom', lambda: controller2.display_questionnaire(fillRoomQuestionnaire()))
-app.run(debug = True,port=3400)
+def fillroom(model):
+    model.add_fillroom(request.form)
+    return render_template('dashboard.html')
+    # return render_template('pass.html',date=date,rent=rent,room=room,occupancy=occupancy,gender=gender,age=age,diet=diet,smoker=smoker,drinker=drinker,music=music,friends=friends)
+
+if __name__ == "__main__":
+    controller1 = Controller()
+    controller2 = Controller.getInstance() #Singleton Pattern
+    controller2.add_url_rules()
+    app.run(debug = True,port=controller1.port)
---
+++ Project5/model.py
@@ -0,0 +1,60 @@
+from pymongo import MongoClient
+class Model:
+    def __init__(self):
+        try:
+            cluster = MongoClient(
+                "mongodb+srv://bunkbud:ooad@cluster0-xythq.mongodb.net/test?retryWrites=true&w=majority")
+            print("Connected!")
+        except:
+            print("Counldnt connect!")
+
+        self.db = cluster["bunkbud"]
+        self.users_collection = self.db["users"]
+        self.findroom_collection = self.db["findroom"]
+        self.fillroom_collection = self.db["fillroom"]
+
+    def add_User(self,record):
+        name = record['name']
+        email = record['email']
+        password = record['password']
+        # description = request.form['description']
+        gender = record['sex']
+        age = record['age']
+        diet = record['diet']
+        smoker = record['smoker']
+        drinker = record['drinker']
+        doc = {"name": name, "email": email, "password": password, "gender": gender, "Age": age, "diet": diet,
+               "smoker": smoker, "drinker":drinker}
+        self.users_collection.insert_one(doc)
+
+    def add_findroom(self,record):
+        date = record['date']
+        rent = record['rent']
+        occupancy = record['tenants']
+        gender = record['sex']
+        age = record['age']
+        diet = record['diet']
+        smoker = record['smoker']
+        drinker = record['drinker']
+        music = record['music']
+        friends = record['friends']
+        doc = {"date": date, "rent": rent, "occupancy": occupancy, "gender": gender, "age": age, "diet": diet,
+               "smoker": smoker, "drinker": drinker, "music": music, "friends": friends}
+        self.findroom_collection.insert_one(doc)
+
+    def add_fillroom(self,record):
+        date = record['date']
+        rent = record['rent']
+        room = record['room']
+        occupancy = record['tenants']
+        gender = record['sex']
+        age = record['age']
+        diet = record['diet']
+        smoker = record['smoker']
+        drinker = record['drinker']
+        music = record['music']
+        friends = record['friends']
+        doc = {"date": date, "rent": rent, "room": room, "occupancy": occupancy, "gender": gender, "age": age,
+               "diet": diet, "smoker": smoker, "drinker": drinker, "music": music, "friends": friends}
+        print("Document to be inserted: ", doc)
+        self.fillroom_collection.insert_one(doc)
